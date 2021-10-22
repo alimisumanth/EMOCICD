5afea46adf34652f1f6ceaa5d60469183da3fb64
alimi <swarthik729@gmail.com>
2021-10-22 13:41:46 +0530

    Adding Files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,7 @@ cd /home/ec2-user/EMOCICD/
 source environment/bin/activate
  {
   cd ~ && wget https://www.sqlite.org/2020/sqlite-autoconf-3320100.tar.gz && tar xvfz sqlite-autoconf-3320100.tar.gz && cd sqlite-autoconf-3320100 && ./configure && make && make install
+cd /home/ec2-user/EMOCICD/
 sudo pip3 install -r requirements.txt
 # collecting static files
 sudo python3 manage.py collectstatic --noinput;
