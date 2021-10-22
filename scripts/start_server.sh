a390392424836cce1000e17cd6c3238609e45515
alimi <swarthik729@gmail.com>
2021-10-22 13:19:31 +0530

    Adding Files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/ec2-user/EMOCICD/
 source environment/bin/activate
-CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
+ {
+
+# collecting static files
+python manage.py collectstatic --noinput;
+# log which migrations have already been applied
+python manage.py showmigrations;
+# migrate the rest
+python manage.py migrate --noinput;
+# another command to create a superuser (write your own)
+}
